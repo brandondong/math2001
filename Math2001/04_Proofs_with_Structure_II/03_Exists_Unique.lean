bc067fdafc0cfa7791dfd7f266656a429db50d6e
Brandon <brandondong96@gmail.com>
2025-09-20 18:50:42 -0700

    Exists unique

--- Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
+++ Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
@@ -21,7 +21,35 @@ example : ∃! a : ℝ, 3 * a + 1 = 7 := by
 
 
 example : ∃! x : ℚ, ∀ a, a ≥ 1 → a ≤ 3 → (a - x) ^ 2 ≤ 1 := by
-  sorry
+  use 2
+  constructor
+  intro x hx1 hx2
+  have h : (x - 2) ^ 2 ≤ 1 ^ 2
+  apply sq_le_sq'
+  addarith [hx1]
+  addarith [hx2]
+  addarith [h]
+  intro y h
+  have h2 : (3 - y) ^ 2 ≤ 1
+  apply h
+  numbers
+  numbers
+  have h3 : (3 - y) ^ 2 ≤ 1 ^ 2 := by addarith [h2]
+  cancel 2 at h3
+  have h4 : (1 - y) ^ 2 ≤ 1
+  apply h
+  numbers
+  numbers
+  have h5 : (1 - y) ^ 2 ≤ 1 ^ 2 := by addarith [h4]
+  have h6 : -1 ≤ 1 - y
+  have h8 : (0: ℚ) ≤ 1 := by numbers
+  have h7 := abs_le_of_sq_le_sq' h5 h8
+  cases' h7 with h7 h8
+  assumption
+  apply le_antisymm
+  addarith [h6]
+  addarith [h3]
+
 
 example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 = x) : x = 0 := by
   obtain ⟨a, ha1, ha2⟩ := hx
@@ -70,10 +98,49 @@ example : ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] := by
 
 
 example : ∃! x : ℚ, 4 * x - 3 = 9 := by
-  sorry
+  use 3
+  constructor
+  ring
+  intro y hy
+  calc
+    y = (4 * y - 3 + 3) / 4 := by ring
+    _ = (9 + 3) / 4 := by rw [hy]
+    _ = 3 := by ring
+
 
 example : ∃! n : ℕ, ∀ a, n ≤ a := by
-  sorry
+  use 0
+  constructor
+  intro x
+  extra
+  intro y hy
+  have h := hy 0
+  interval_cases y
+  ring
+
 
 example : ∃! r : ℤ, 0 ≤ r ∧ r < 3 ∧ 11 ≡ r [ZMOD 3] := by
-  sorry
+  use 2
+  constructor
+  constructor
+  numbers
+  constructor
+  numbers
+  use 3
+  ring
+  intro y hy
+  cases' hy with hy1 hy2
+  cases' hy2 with hy2 hy3
+  cases' hy3 with x hx
+  have h2 := calc
+    3 * x = 11 - y := by addarith [hx]
+    _ ≤ 11 - 0 := by rel [hy1]
+    _ < 3 * 4 := by numbers
+  cancel 3 at h2
+  have h3 := calc
+    3 * x = 11 - y := by addarith [hx]
+    _ > 11 - 3 := by rel [hy2]
+    _ > 3 * 2 := by numbers
+  cancel 3 at h3
+  interval_cases x
+  addarith [hx]
---
+++ theorems.md
@@ -0,0 +1,4 @@
+* eq_zero_or_eq_zero_of_mul_eq_zero: a * b = 0 → a = 0 ∨ b = 0
+* abs_le_of_sq_le_sq': x ^ 2 ≤ y ^ 2 → -y ≤ x ∧ x ≤ y
+* sq_le_sq': -y ≤ x, x ≤ y → x ^ 2 ≤ y ^ 2
+* le_antisymm: a ≤ b → b ≤ a → a = b
