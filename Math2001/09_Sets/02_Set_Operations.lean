b821a5cee4fa5d950402be7b22dd1451479eb514
Brandon <brandondong96@gmail.com>
2025-10-05 13:52:14 -0700

    It's all number theory

--- Math2001/09_Sets/02_Set_Operations.lean
+++ Math2001/09_Sets/02_Set_Operations.lean
@@ -32,9 +32,24 @@ example : {1, 2} ∪ {2, 4} = {1, 2, 4} := by
       left
       apply h
   -- and much, much more
-    · sorry
-    · sorry
-  · sorry
+    · right
+      left
+      assumption
+    · right
+      right
+      assumption
+  · intro h
+    cases' h with h h
+    . left
+      left
+      apply h
+    cases' h with h h
+    . left
+      right
+      apply h
+    right
+    right
+    assumption
 
 
 example : {2, 1} ∪ {2, 4} = {1, 2, 4} := by
@@ -129,26 +144,80 @@ example : {x : ℝ | -1 < x} ∪ {x : ℝ | x < 1} = univ := by
 macro "check_equality_of_explicit_sets" : tactic => `(tactic| (ext; dsimp; exhaust))
 
 
-example : {-1, 2, 4, 4} ∪ {3, -2, 2} = sorry := by check_equality_of_explicit_sets
+example : {-1, 2, 4, 4} ∪ {3, -2, 2} = {-1, 4, 3, -2, 2} := by check_equality_of_explicit_sets
 
-example : {0, 1, 2, 3, 4} ∩ {0, 2, 4, 6, 8} = sorry := by
+example : {0, 1, 2, 3, 4} ∩ {0, 2, 4, 6, 8} = {0, 2, 4} := by
   check_equality_of_explicit_sets
 
-example : {1, 2} ∩ {3} = sorry := by check_equality_of_explicit_sets
+example : {1, 2} ∩ {3} = {} := by check_equality_of_explicit_sets
 
-example : {3, 4, 5}ᶜ ∩ {1, 3, 5, 7, 9} = sorry := by
+example : {3, 4, 5}ᶜ ∩ {1, 3, 5, 7, 9} = {1, 7, 9} := by
   check_equality_of_explicit_sets
 
 example : {r : ℤ | r ≡ 7 [ZMOD 10] }
     ⊆ {s : ℤ | s ≡ 1 [ZMOD 2]} ∩ {t : ℤ | t ≡ 2 [ZMOD 5]} := by
-  sorry
+  intro a ha
+  dsimp at *
+  cases' ha with x hx
+  have H : a = 10 * x + 7 := by addarith [hx]
+  clear hx
+  constructor
+  . use a - 5 * x - 4
+    rw [H]
+    ring
+  . use 2 * x + 1
+    rw [H]
+    ring
+
 
 example : {n : ℤ | 5 ∣ n} ∩ {n : ℤ | 8 ∣ n} ⊆ {n : ℤ | 40 ∣ n} := by
-  sorry
+  intro n hn
+  cases' hn with hn5 hn8
+  dsimp at *
+  cases' hn5 with x hx
+  cases' hn8 with y hy
+  use 2 * x - 3 * y
+  calc
+    n = 16 * n - 15 * n := by ring
+    _ = 16 * (5 * x) - 15 * n := by rw [hx]
+    _ = 16 * (5 * x) - 15 * (8 * y) := by rw [hy]
+    _ = 40 * (2 * x - 3 * y) := by ring
+
 
 example :
     {n : ℤ | 3 ∣ n} ∪ {n : ℤ | 2 ∣ n} ⊆ {n : ℤ | n ^ 2 ≡ 1 [ZMOD 6]}ᶜ := by
-  sorry
+  intro n
+  dsimp
+  intro hn
+  intro contra
+  cases' contra with y hy
+  cases' hn with hn hn <;> cases' hn with x hx
+  . rw [hx] at hy
+    clear hx n
+    have h : (3 * x) ^ 2 -6 * y = 1 := by addarith [hy]
+    have temp : (3 * x) ^ 2 -6 * y = 3 * (3 * x ^ 2 - 2 * y) := by ring
+    rw [temp] at h
+    clear temp hy
+    have contra : ¬(3: ℤ) ∣ 1
+    . apply Int.not_dvd_of_exists_lt_and_lt
+      use 0
+      constructor <;> numbers
+    apply contra
+    use (3 * x ^ 2 - 2 * y)
+    rw [h]
+  . have goal1 : Int.Even (n ^ 2 - 1)
+    . use 3 * y
+      rw [hy]
+      ring
+    have goal2 : Int.Odd (n ^ 2 - 1)
+    . rw [hx]
+      clear goal1 hx hy
+      use 2*x^2 - 1
+      ring
+    clear hy x hx
+    have contra : ¬ Int.Even (n ^ 2 - 1) := by exact (Int.odd_iff_not_even (n ^ 2 - 1)).mp goal2
+    contradiction
+
 
 def SizeAtLeastTwo (s : Set X) : Prop := ∃ x1 x2 : X, x1 ≠ x2 ∧ x1 ∈ s ∧ x2 ∈ s
 def SizeAtLeastThree (s : Set X) : Prop :=
@@ -157,4 +226,34 @@ def SizeAtLeastThree (s : Set X) : Prop :=
 example {s t : Set X} (hs : SizeAtLeastTwo s) (ht : SizeAtLeastTwo t)
     (hst : ¬ SizeAtLeastTwo (s ∩ t)) :
     SizeAtLeastThree (s ∪ t) := by
-  sorry
+  cases' hs with s1 hs1
+  cases' hs1 with s2 hs
+  cases' hs with hs_ne hs
+  cases' hs with hs1 hs2
+  cases' ht with t1 ht1
+  cases' ht1 with t2 ht
+  cases' ht with ht_ne ht
+  cases' ht with ht1 ht2
+  dsimp [SizeAtLeastTwo] at hst
+  dsimp [SizeAtLeastThree]
+  by_cases hs1t : s1 ∈ t <;> by_cases hs2t : s2 ∈ t <;> by_cases ht1s : t1 ∈ s <;> by_cases ht2s : t2 ∈ s
+  . exfalso
+    apply hst
+    use s1, s2
+    exhaust
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . sorry
+  . use s1, s2, t1
+    exhaust
