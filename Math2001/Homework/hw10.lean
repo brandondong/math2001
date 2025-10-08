9052b5873680cf699d430cf2c59fed594e881e59
Brandon <brandondong96@gmail.com>
2025-10-07 20:44:37 -0700

    Finish!

--- Math2001/Homework/hw10.lean
+++ Math2001/Homework/hw10.lean
@@ -19,34 +19,51 @@ for clearer statements and any special instructions. -/
 
 @[autograded 4]
 theorem problem1a : { m : ℤ | m ≥ 10 } ⊆ { n : ℤ | n ^ 3 - 6 * n ^ 2 ≥ 4 * n } := by
-  sorry
-
-@[autograded 4]
-theorem problem1b : { m : ℤ | m ≥ 10 } ⊈ { n : ℤ | n ^ 3 - 6 * n ^ 2 ≥ 4 * n } := by
-  sorry
+  intro m hm
+  dsimp at *
+  calc
+    m ^ 3 - 6 * m ^ 2 = m * m ^ 2 - 6 * m ^ 2 := by ring
+    _ ≥ 10 * m ^ 2 - 6 * m ^ 2 := by rel [hm]
+    _ = 4 * m * m := by ring
+    _ ≥ 4 * 10 * m := by rel [hm]
+    _ = 4 * m + 36 * m := by ring
+    _ ≥ 4 * m := by extra
 
 
 /- Problem 2: prove one of these, delete the other -/
 
 @[autograded 3]
-theorem problem2a : { t : ℝ | t ^ 2 - 3 * t + 2 = 0 } = { s : ℝ | s = 2 } := by
-  sorry
-
-@[autograded 3]
 theorem problem2b : { t : ℝ | t ^ 2 - 3 * t + 2 = 0 } ≠ { s : ℝ | s = 2 } := by
-  sorry
+  ext
+  push_neg
+  use 1
+  left
+  constructor
+  . dsimp
+    ring
+  . dsimp
+    numbers
 
 
 /- Problem 3: prove one of these, delete the other -/
 
 @[autograded 3]
 theorem problem3a : {1, 2, 3} ∩ {2, 3, 4} ⊆ {2, 3, 6} := by
-  sorry
-
-
-@[autograded 3]
-theorem problem3b : ¬ {1, 2, 3} ∩ {2, 3, 4} ⊆ {2, 3, 6} := by
-  sorry
+  intro a ha
+  dsimp at *
+  cases' ha with ha1 ha2
+  cases' ha1 with ha1 ha1 <;> cases' ha2 with ha2 ha2
+  . left
+    assumption
+  . cases' ha2 with ha2 ha2 <;> rw [ha2] at ha1 <;> numbers at ha1
+  . left
+    assumption
+  . cases' ha1 with ha1 ha1
+    . left
+      assumption
+    . right
+      left
+      assumption
 
 
 /- Problem 4 -/
@@ -54,8 +71,27 @@ theorem problem3b : ¬ {1, 2, 3} ∩ {2, 3, 4} ⊆ {2, 3, 6} := by
 @[autograded 4]
 theorem problem4 : { r : ℤ | r ≡ 11 [ZMOD 15] }
     = { s : ℤ | s ≡ 2 [ZMOD 3] } ∩ { t : ℤ | t ≡ 1 [ZMOD 5] } := by
-  sorry
-
+  ext x
+  constructor <;> intro h <;> dsimp at *
+  . cases' h with y hy
+    have hx : x = 15 * y + 11 := by addarith [hy]
+    rw [hx]
+    constructor
+    . use 5 * y + 3
+      ring
+    . use 3 * y + 2
+      ring
+  . cases' h with h1 h2
+    cases' h1 with a ha
+    cases' h2 with b hb
+    have ha2 : x = 3 * a + 2 := by addarith [ha]
+    have hb2 : x = 5 * b + 1 := by addarith [hb]
+    use 2 * b - a - 1
+    calc
+      x - 11 = 6 * x - 5 * x - 11 := by ring
+      _ = 6 * x - 5 * (3 * a + 2) - 11 := by rw [ha2]
+      _ = 6 * (5 * b + 1) - 5 * (3 * a + 2) - 11 := by rw [hb2]
+      _ = _ := by ring
 
 
 /-! ### Problem 5 starts here -/
@@ -68,45 +104,85 @@ local infix:50 "∼" => fun (a b : ℤ) ↦ ∃ m n, m > 0 ∧ n > 0 ∧ a * m =
 
 @[autograded 2]
 theorem problem51a : Reflexive (· ∼ ·) := by
-  sorry
-
-@[autograded 2]
-theorem problem51b : ¬ Reflexive (· ∼ ·) := by
-  sorry
+  dsimp [Reflexive]
+  intro x
+  use 1, 1
+  constructor
+  . numbers
+  constructor
+  . numbers
+  rfl
 
 
 /- Problem 5.2: prove one of these, delete the other -/
 
 @[autograded 2]
 theorem problem52a : Symmetric (· ∼ ·) := by
-  sorry
-
-@[autograded 2]
-theorem problem52b : ¬ Symmetric (· ∼ ·) := by
-  sorry
+  dsimp [Symmetric]
+  intro x y h
+  cases' h with m h
+  cases' h with n h
+  use n, m
+  constructor
+  . apply h.right.left
+  constructor
+  . apply h.left
+  rw [h.right.right]
 
 
 /- Problem 5.3: prove one of these, delete the other -/
 
 @[autograded 2]
-theorem problem53a : AntiSymmetric (· ∼ ·) := by
-  sorry
-
-@[autograded 2]
 theorem problem53b : ¬ AntiSymmetric (· ∼ ·) := by
-  sorry
+  dsimp [AntiSymmetric]
+  push_neg
+  use 1, 2
+  constructor
+  . use 2, 1
+    constructor
+    . numbers
+    constructor
+    . numbers
+    ring
+  constructor
+  . use 1, 2
+    constructor
+    . numbers
+    constructor
+    . numbers
+    ring
+  numbers
 
 
 /- Problem 5.4: prove one of these, delete the other -/
 
 @[autograded 2]
 theorem problem54a : Transitive (· ∼ ·) := by
-  sorry
-
-@[autograded 2]
-theorem problem54b : ¬ Transitive (· ∼ ·) := by
-  sorry
-
+  dsimp [Transitive]
+  intro x y z hxy hyz
+  cases' hxy with a hxy
+  cases' hxy with b hxy
+  cases' hyz with c hyz
+  cases' hyz with d hyz
+  use a * c, b * d
+  cases' hxy with ha hxy
+  cases' hyz with hc hyz
+  cases' hxy with hb hxy
+  cases' hyz with hd hyz
+  constructor
+  . calc
+      a * c > a * 0 := by rel [hc]
+      _ = 0 := by ring
+  constructor
+  . calc
+      b * d > b * 0 := by rel [hd]
+      _ = 0 := by ring
+  calc
+    x * (a * c) = x * a * c := by ring
+    _ = y * b * c := by rw [hxy]
+    _ = y * c * b := by ring
+    _ = z * d * b := by rw [hyz]
+    _ = _ := by ring
 
 
 /-! ### Problem 6 starts here -/
@@ -118,41 +194,47 @@ infix:50 "≺" => fun ((x1, y1) : ℝ × ℝ) (x2, y2) ↦ (x1 ≤ x2 ∧ y1 ≤
 
 @[autograded 2]
 theorem problem61a : Reflexive (· ≺ ·) := by
-  sorry
-
-@[autograded 2]
-theorem problem61b : ¬ Reflexive (· ≺ ·) := by
-  sorry
+  dsimp [Reflexive]
+  intro x
+  constructor <;> extra
 
 
 /- Problem 6.2: prove one of these, delete the other -/
 
 @[autograded 2]
-theorem problem62a : Symmetric (· ≺ ·) := by
-  sorry
-
-@[autograded 2]
 theorem problem62b : ¬ Symmetric (· ≺ ·) := by
-  sorry
+  dsimp [Symmetric]
+  push_neg
+  use (1,1), (2,2)
+  constructor
+  . constructor <;> numbers
+  . left
+    numbers
 
 
 /- Problem 6.3: prove one of these, delete the other -/
 
 @[autograded 2]
 theorem problem63a : AntiSymmetric (· ≺ ·) := by
-  sorry
-
-@[autograded 2]
-theorem problem63b : ¬ AntiSymmetric (· ≺ ·) := by
-  sorry
+  dsimp [AntiSymmetric]
+  intro x y hx hy
+  constructor <;> apply le_antisymm
+  . apply hx.left
+  . apply hy.left
+  . apply hx.right
+  . apply hy.right
 
 
 /- Problem 6.4: prove one of these, delete the other -/
 
 @[autograded 2]
 theorem problem64a : Transitive (· ≺ ·) := by
-  sorry
-
-@[autograded 2]
-theorem problem64b : ¬ Transitive (· ≺ ·) := by
-  sorry
+  dsimp [Transitive]
+  intro x y z hx hy
+  constructor
+  . calc
+      x.1 ≤ y.1 := hx.left
+      _ ≤ z.1 := hy.left
+  . calc
+      x.2 ≤ y.2 := hx.right
+      _ ≤ z.2 := hy.right
--- Math2001/Homework/hw9.lean
+++ Math2001/Homework/hw9.lean
@@ -21,65 +21,82 @@ for clearer statements and any special instructions. -/
 
 @[autograded 4]
 theorem problem1a : Surjective (fun (x : ℝ) ↦ 2 * x) := by
-  sorry
-
-@[autograded 4]
-theorem problem1b : ¬ Surjective (fun (x : ℝ) ↦ 2 * x) := by
-  sorry
+  dsimp [Surjective]
+  intro b
+  use b/2
+  ring
 
 
 /- Problem 2: prove one of these, delete the other -/
 
 @[autograded 4]
-theorem problem2a : Surjective (fun (x : ℤ) ↦ 2 * x) := by
-  sorry
-
-@[autograded 4]
 theorem problem2b : ¬ Surjective (fun (x : ℤ) ↦ 2 * x) := by
-  sorry
+  dsimp [Surjective]
+  push_neg
+  use 1
+  have f : ¬(2: ℤ) ∣ 1
+  . apply Int.not_dvd_of_exists_lt_and_lt
+    use 0
+    constructor <;> numbers
+  intro a
+  intro h
+  apply f
+  use a
+  rw [h]
 
 
 /- Problem 3: prove one of these, delete the other -/
 
 @[autograded 4]
 theorem problem3a : ∀ (f : ℚ → ℚ), Injective f → Injective (fun x ↦ f x + 1) := by
-  sorry
-
-@[autograded 4]
-theorem problem3b : ¬ ∀ (f : ℚ → ℚ), Injective f → Injective (fun x ↦ f x + 1) := by
-  sorry
+  intro f hf
+  dsimp [Injective] at *
+  intro a1 a2 ha
+  apply hf
+  addarith [ha]
 
 
 /- Problem 4: prove one of these, delete the other -/
 
 @[autograded 4]
 theorem problem4a : Bijective (fun (x : ℝ) ↦ 3 - 2 * x) := by
-  sorry
-
-@[autograded 4]
-theorem problem4b : ¬ Bijective (fun (x : ℝ) ↦ 3 - 2 * x) := by
-  sorry
+  constructor
+  . dsimp [Injective]
+    intro a1 a2 ha
+    have temp : -2 * a1 = -2 * a2 := by addarith [ha]
+    cancel -2 at temp
+  . dsimp [Surjective]
+    intro b
+    use (b - 3)/(-2)
+    ring
 
 
 /- Problem 5: prove one of these, delete the other -/
 
 @[autograded 5]
-theorem problem5a :
-    Injective (fun ((x, y, z) : ℝ × ℝ × ℝ) ↦ (x + y + z, x + 2 * y + 3 * z)) := by
-  sorry
-
-@[autograded 5]
 theorem problem5b :
     ¬Injective (fun ((x, y, z) : ℝ × ℝ × ℝ) ↦ (x + y + z, x + 2 * y + 3 * z)) := by
-  sorry
+  dsimp [Injective]
+  push_neg
+  use (1,1,1), (0,3,0)
+  constructor
+  . constructor <;> ring
+  numbers
 
 
 /- Problem 6: prove one of these, delete the other -/
 
 @[autograded 4]
 theorem problem6a : Bijective (fun ((r, s) : ℚ × ℚ) ↦ (s, r + 2 * s)) := by
-  sorry
-
-@[autograded 4]
-theorem problem6b : ¬ Bijective (fun ((r, s) : ℚ × ℚ) ↦ (s, r + 2 * s)) := by
-  sorry
+  constructor
+  . dsimp [Injective]
+    intro a1 a2 ha
+    obtain ⟨hm1, hm2⟩ := ha
+    constructor
+    . rw [hm1] at hm2
+      addarith [hm2]
+    . apply hm1
+  . dsimp [Surjective]
+    intro b
+    use (b.2 - 2 * b.1, b.1)
+    constructor <;> ring
